543403a2320f4b1523b3985579ebb9694d80149b
BraianFranco <BFraanco66@gmail.com>
2024-09-22 18:56:56 -0300

    Primera subida.

---
+++ codigo.js
@@ -0,0 +1,9 @@
+
+         function FormularioMensaje(){
+             alert("Gracias "+ document.getElementById("txtNombre").value +" por contactarme , en unos instantes se le contestara... ");
+             document.getElementById("txtNombre").value = "";
+             document.getElementById("txtCorreo").value = "";
+             document.getElementById("txtTel").value = "";
+             document.getElementById("txtMensaje").value = "";
+         }
+ 
